ccc44bb1ec490c5c516104225def5ee3aee0525f
pabloX <lonwabo.mnyaiza@gmail.com>
2026-05-26 20:17:09 +0200

    :card_file_box: hydrate membershiptypes.

---
+++ MovieHub.API/Migrations/20260526180455_HydrateMembershipTypes.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieHub.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class HydrateMembershipTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            Sql(@"insert into MembershipType 
+                (SignUpFee, DurationInMonth, DiscountRate)
+                values 
+                (1, 0, 0, 0),
+                (2, 30, 1, 10),
+                (3, 90, 3, 15),
+                (4, 300, 12, 15)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            Sql("delete * from MemberShipType");
+        }
+    }
+}
--- MovieHub.API/Models/Customer.cs
+++ MovieHub.API/Models/Customer.cs
@@ -4,4 +4,7 @@ public class Customer
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    public bool IsSubscribed { get; set; }
+    public MembershipType MembershipType { get; set; }
+    public byte MembershipTypeId { get; set; }
 }
