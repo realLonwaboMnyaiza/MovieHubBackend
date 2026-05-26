2ecb65a9776579c1c00886dc66f2b4e8cd02c9ba
pabloX <lonwabo.mnyaiza@gmail.com>
2026-05-26 20:26:49 +0200

    :card_file_box::zap: add constraints on models.

--- MovieHub.API/Models/Customer.cs
+++ MovieHub.API/Models/Customer.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MovieHub.API.Models;
 
 public class Customer
 {
     public int Id { get; set; }
+    [Required]
+    [StringLength(255)]
     public string Name { get; set; }
     public bool IsSubscribed { get; set; }
     public MembershipType MembershipType { get; set; }
