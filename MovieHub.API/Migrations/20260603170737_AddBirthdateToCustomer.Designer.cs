c745fb149bac54fe694c38ffc7caeb9a5521a743
pabloX <lonwabo.mnyaiza@gmail.com>
2026-06-03 19:16:48 +0200

    :card_file_box: update database tables for customer.

--- MovieHub.API/Migrations/20260603170737_AddBirthdateToCustomer.Designer.cs
+++ MovieHub.API/Migrations/20260603170737_AddBirthdateToCustomer.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MovieHub.API.Data;
 
@@ -11,9 +12,11 @@ using MovieHub.API.Data;
 namespace MovieHub.API.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260603170737_AddBirthdateToCustomer")]
+    partial class AddBirthdateToCustomer
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
