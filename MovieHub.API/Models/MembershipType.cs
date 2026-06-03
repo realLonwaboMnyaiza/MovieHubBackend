c7414604a8bbd4173a6f5d75a064674d7ad13f79
pabloX <lonwabo.mnyaiza@gmail.com>
2026-06-03 12:59:34 +0200

    :package: move api into its own project.

--- MovieHub.API/Models/MembershipType.cs
+++
@@ -1,9 +0,0 @@
-namespace MovieHub.API.Models;
-
-public class MembershipType 
-{
-   public byte Id { get; set; }
-   public short SignUpFee { get; set;}
-   public byte DurationInMonths { get; set; }
-   public byte DiscountRate { get; set; }
-}
