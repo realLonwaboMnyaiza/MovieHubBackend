0256de384649ecdd1cd2e78670cdd0c63e6dd081
pabloX <lonwabo.mnyaiza@gmail.com>
2026-06-03 14:15:04 +0200

    :safety_vest: add custom form validation for members.

---
+++ MovieHub.Web/Models/AgeVerification.cs
@@ -0,0 +1,22 @@
+
+namespace MovieHub.Web.Models;
+
+public class AgeVerification : ValidationAttribute
+{
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        var customer = validationContext.ObjectInstance as Customer;
+
+        if (customer.MembershipTypeId == 0 || customer.MembershipTypeId == 1)
+            return ValidationResult.Success;
+
+        if (customers.Birthdate is null)
+            return new ValidationResult("Birthdate is required.");
+
+        var age = DateTime.Today.Year - customer.Birthdate.Value.Year;
+
+        return (age >= 18) 
+            ? ValidationResult.Success 
+            : new ValidationResult("Customer must meet age verification requirements to register as a member.")
+    }
+}
