bdbe829298a14dd70e49b90261411c244e2844a5
pabloX <lonwabo.mnyaiza@gmail.com>
2026-06-03 18:55:59 +0200

    :card_file_box: create database migrations code first.

---
+++ MovieHub.API/Migrations/20260526174246_RenameTableToCustomers.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieHub.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameTableToCustomers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Products",
+                table: "Products");
+
+            migrationBuilder.RenameTable(
+                name: "Products",
+                newName: "Customers");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Customers",
+                table: "Customers",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Customers",
+                table: "Customers");
+
+            migrationBuilder.RenameTable(
+                name: "Customers",
+                newName: "Products");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Products",
+                table: "Products",
+                column: "Id");
+        }
+    }
+}
---
+++ MovieHub.API/Migrations/20260526180455_HydrateMembershipTypes.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieHub.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class HydrateMembershipTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"insert into MembershipType 
+                (Id, SignUpFee, DurationInMonths, DiscountRate)
+                values 
+                (1, 0, 0, 0),
+                (2, 30, 1, 10),
+                (3, 90, 3, 15),
+                (4, 300, 12, 15)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("delete * from MemberShipType where Id in (1,2,3,4)");
+        }
+    }
+}
